4d44f2ea44bf7fe2b8ec73aebff1228fefaa54ee
xgeeek <xugang6852@gmail.com>
2024-11-11 09:13:10 +0800

    feat: 资源更新；

--- app/src/main/java/com/advance/kotlin/AdvApplication.kt
+++ app/src/main/java/com/advance/kotlin/AdvApplication.kt
@@ -4,6 +4,10 @@ import android.app.Application
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+import androidx.lifecycle.ProcessLifecycleOwner
 import com.blankj.utilcode.util.Utils
 import com.tencent.mmkv.MMKV
 
@@ -25,5 +29,22 @@ class AdvApplication : Application() {
         // /data/user/0/项目包名/files/mmkv
         val initialize = MMKV.initialize(this)
         Log.d("mmkv", initialize)
+
+        ProcessLifecycleOwner.get().lifecycle.addObserver(ApplicationLifecycleObserver())
+    }
+
+
+    class ApplicationLifecycleObserver : LifecycleObserver {
+
+        @OnLifecycleEvent(Lifecycle.Event.ON_START)
+        fun onAppForeground() {
+            Log.d("lifecycleApp", "app foreground")
+        }
+
+        @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+        fun onAppBackground() {
+            Log.d("lifecycleApp", "app background")
+        }
+
     }
 }
